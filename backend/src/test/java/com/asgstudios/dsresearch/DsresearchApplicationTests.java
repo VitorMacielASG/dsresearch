da676fadc57bf42f519520b69a630fdc329bf65a
Vitor Maciel <engvitormaciel@gmail.com>
2020-09-14 17:25:32 -0300

    Projeto created

---
+++ backend/src/test/java/com/asgstudios/dsresearch/DsresearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asgstudios.dsresearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsresearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
