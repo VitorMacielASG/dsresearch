4e3627575a4582496a0a01d846cb960bf7c70ae6
Vitor Maciel <engvitormaciel@gmail.com>
2020-09-14 19:54:17 -0300

    POST records

---
+++ backend/src/main/java/com/asgstudios/dsresearch/services/RecordService.java
@@ -0,0 +1,45 @@
+package com.asgstudios.dsresearch.services;
+
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.asgstudios.dsresearch.dto.RecordDTO;
+import com.asgstudios.dsresearch.dto.RecordInsertDTO;
+import com.asgstudios.dsresearch.entities.Game;
+import com.asgstudios.dsresearch.entities.Record;
+import com.asgstudios.dsresearch.repositories.GameRepository;
+import com.asgstudios.dsresearch.repositories.RecordRepository;
+
+
+@Service
+public class RecordService {
+
+	@Autowired
+	private RecordRepository repository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+		
+		Record entity = new Record();
+		
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());
+		entity.setMoment(Instant.now());
+		
+		Game game = gameRepository.getOne(dto.getGameId());
+		entity.setGame(game);
+		
+		entity = repository.save(entity);
+		
+		return new RecordDTO(entity);
+	}
+	
+}
