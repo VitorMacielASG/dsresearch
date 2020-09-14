043da09fe756f4f2a36c4b91ed19772923a0dbf9
Vitor Maciel <engvitormaciel@gmail.com>
2020-09-14 20:30:28 -0300

    GET Records

--- backend/src/main/java/com/asgstudios/dsresearch/services/RecordService.java
+++ backend/src/main/java/com/asgstudios/dsresearch/services/RecordService.java
@@ -4,6 +4,8 @@ package com.asgstudios.dsresearch.services;
 import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -40,6 +42,13 @@ public class RecordService {
 		entity = repository.save(entity);
 		
 		return new RecordDTO(entity);
+	}
+
+
+	@Transactional(readOnly = true)
+	public Page<RecordDTO> findByMoments(Instant minDate, Instant maxDate, PageRequest pageRequest) {
+		
+		return repository.findByMoments(minDate, maxDate, pageRequest).map(x -> new RecordDTO(x));
 	}
 	
 }
